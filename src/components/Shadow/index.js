a3d1c8eda579d26b919c1b3abce36d3c0d9ff9c3
filippinifra <filippinifra123@gmail.com>
2021-01-13 19:26:18 +0100

    Creade Shadow Element

---
+++ src/components/Shadow/index.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const ShadowStyle = StyleSheet.create({
+  ShadowBox: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+});
