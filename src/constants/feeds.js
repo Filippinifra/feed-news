fdec13123545f188996d18a5e8bf916723a89d3d
filippinifra <filippinifra123@gmail.com>
2021-01-13 19:27:05 +0100

    Add feeds and colors

---
+++ src/constants/feeds.js
@@ -0,0 +1,13 @@
+export const feeds = [
+  { name: "Techeme", url: "https://www.techmeme.com/feed.xml?x=1" },
+  { name: "Tech Crunch", url: "https://feeds.feedburner.com/TechCrunch" },
+  { name: "Technology Review", url: "https://www.technologyreview.com/feed/" },
+  {
+    name: "Technology Lab",
+    url: "http://feeds.arstechnica.com/arstechnica/technology-lab",
+  },
+  {
+    name: "Venture Beat",
+    url: "https://feeds.feedburner.com/venturebeat/SZYF",
+  },
+];
---
+++ src/constants/palette.js
@@ -0,0 +1,15 @@
+export const COMMON_FIRST_COLOR = "#ffffff";
+export const COMMON_SECOND_COLOR = "lightgray";
+export const COMMON_ERROR_COLOR = "red";
+
+export const FIRST_COLOR = "#007DF7"; //BLUE
+
+export const RAINBOW_COLORS = [
+  "red",
+  "orange",
+  "#FFC300",
+  "limegreen",
+  "green",
+  "blue",
+  "purple",
+];
