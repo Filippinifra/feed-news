20f00a041801e7a8e3b39ba5db4d662ff966ec2d
filippinifra <filippinifra123@gmail.com>
2021-01-13 19:25:14 +0100

    Add configuration

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            components: "./src/components",
+            constants: "./src/constants",
+            hook: "./src/hook",
+            images: "./src/images",
+            screen: "./src/screen",
+            utils: "./src/utils",
+          },
+        },
+      ],
+    ],
   };
 };
