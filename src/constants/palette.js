c0cf5f8a92662d4e9f17a8704ca450805550a424
filippinifra <filippinifra123@gmail.com>
2021-01-14 01:03:33 +0100

    Add new colors and reorder feed items

--- src/constants/palette.js
+++ src/constants/palette.js
@@ -3,6 +3,8 @@ export const COMMON_SECOND_COLOR = "lightgray";
 export const COMMON_ERROR_COLOR = "red";
 
 export const FIRST_COLOR = "#007DF7"; //BLUE
+export const SECOND_COLOR = "orange";
+export const THIRD_COLOR = "red";
 
 export const RAINBOW_COLORS = [
   "red",
