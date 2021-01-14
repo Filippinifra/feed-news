f2e83211ea579ff2594f3e6ff2ddb519b019e847
filippinifra <filippinifra123@gmail.com>
2021-01-14 01:03:23 +0100

    Rework router

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -1,22 +1,58 @@
 import React from "react";
 import { MainLayout } from "components/MainLayout";
 import { News } from "screen/News";
-import { FIRST_COLOR } from "constants/palette";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  COMMON_FIRST_COLOR,
+  FIRST_COLOR,
+  THIRD_COLOR,
+} from "constants/palette";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItem,
+} from "@react-navigation/drawer";
 import { feeds } from "constants/feeds";
-
 import { NavigationContainer } from "@react-navigation/native";
+import { Title, ButtonAddNew, ButtonSaved } from "./styles";
+import { Icon } from "react-native-elements";
 
 const Drawer = createDrawerNavigator();
 
+const CustomDrawerContent = (props) => {
+  const { navigation } = props;
+  return (
+    <DrawerContentScrollView {...props}>
+      <Title>My feed list</Title>
+
+      {feeds.map(({ name }, index) => (
+        <DrawerItem
+          {...props}
+          label={name}
+          onPress={() => navigation.navigate(name)}
+          style={{ backgroundColor: FIRST_COLOR, opacity: 0.4 }}
+          labelStyle={{ color: COMMON_FIRST_COLOR }}
+          key={`drawer-item-${index}`}
+        />
+      ))}
+      <ButtonAddNew>
+        <Icon name="add" color={COMMON_FIRST_COLOR}></Icon>
+      </ButtonAddNew>
+      <Title>My saved news</Title>
+      <ButtonSaved name="bookmark" color={THIRD_COLOR}></ButtonSaved>
+    </DrawerContentScrollView>
+  );
+};
+
 export const Router = () => (
   <NavigationContainer>
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+    >
       {feeds.map(({ name, url }, index) => (
         <Drawer.Screen name={name} key={`drawer-${index}`}>
           {() => (
             <MainLayout colorHeader={FIRST_COLOR}>
-              <News mainColor={FIRST_COLOR} url={url} />
+              <News mainColor={FIRST_COLOR} url={url} nameFeed={name} />
             </MainLayout>
           )}
         </Drawer.Screen>
---
+++ src/components/Router/styles.js
@@ -0,0 +1,26 @@
+import { TouchElement } from "components/TouchElement";
+import { FIRST_COLOR, SECOND_COLOR } from "constants/palette";
+import { Icon } from "react-native-elements";
+import styled from "styled-components";
+
+export const Title = styled.Text`
+  font-weight: 800;
+  font-size: 26px;
+  padding: 20px 10px 10px 10px;
+`;
+
+export const ButtonAddNew = styled(TouchElement)`
+  background-color: ${FIRST_COLOR};
+  margin: 10px 10px 10px 10px;
+  border-radius: 4px;
+  padding: 5px;
+`;
+
+export const ButtonSaved = styled(Icon)`
+  margin: 10px 10px 10px 10px;
+  border-radius: 4px;
+  padding: 5px;
+  background-color: ${SECOND_COLOR};
+  top: 0;
+  right: 0;
+`;
