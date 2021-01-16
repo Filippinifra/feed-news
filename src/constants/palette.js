e1d95c7731da015338ae05afb6c660219c1fb605
filippinifra <filippinifra123@gmail.com>
2021-01-16 19:38:45 +0100

    Adding new color

--- src/constants/palette.js
+++ src/constants/palette.js
@@ -1,5 +1,6 @@
 export const COMMON_FIRST_COLOR = "#ffffff";
 export const COMMON_SECOND_COLOR = "lightgray";
+export const COMMON_THIRD_COLOR = "darkgrey";
 export const COMMON_ERROR_COLOR = "red";
 
 export const FIRST_COLOR = "#007DF7"; //BLUE
